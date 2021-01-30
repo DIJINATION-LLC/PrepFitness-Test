5c316fdd994981e2a33756386ee150d30e670296
dsmartsolutions <32335263+dsmartsolutions@users.noreply.github.com>
2021-01-30 15:14:35 +0500

    Update rss.js

--- js/rss.js
+++ js/rss.js
@@ -54,9 +54,15 @@ function loadRSS(link, htmlContainer, linktext) {
       var entries = result.feed.entries;
       for (var i = 0; i < 12; i++) {
         var entry = entries[i];
+        var desc = entry.description;
+        var el = document.createElement( 'html' );
+        el.innerHTML = desc;
+        //el.innerHTML = '<description><div class="hs-featured-image-wrapper"> <a href="https://www.afpafitness.com/blog/the-role-of-amino-acids-in-promoting-health" title="" class="hs-featured-image-link"> <img src="https://www.afpafitness.com/hubfs/image-from-rawpixel-id-86097-jpeg%20%281%29-1.jpg" alt="The Role of Amino Acids in Promoting Health&nbsp;" class="hs-featured-image" style="width:auto !important; max-width:50%; float:left; margin:0 15px 15px 0;"> </a> </div> <p>While plenty of research has been carried out on the importance of an adequate role of complete proteins, over the past several decades researchers have had a growing interest in the specific roles of protein building blocks, called amino acids, in overall health, metabolism, body composition, and sports performance.</p></description>'
+        var imgSrc = document.getElementById("mydiv").append( el.getElementsByTagName( 'img' )[0]);
         container.append(
           '<div class="owl-item"><div class="col-md-4 col-sm-6"><div class="card RSS"><img class="card-img-top img-fluid" src="' +
-            entry.enclosures[0].url +
+            //entry.enclosures[0].url +
+            imgSrc +
             '"><div class="card-body"><h5><a href="' +
             entry.link +
             '"  target="_blank" class="stretched-link">' +
