bbd9b2f1b3e6ed52f001f16b8ffa36c69e3bf3df
Taimur Israr <taimurisrar806@gmail.com>
2021-03-15 23:26:29 +0500

    Update rss.js

--- js/rss.js
+++ js/rss.js
@@ -8,9 +8,12 @@ $(document).ready(function() {
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 $(document).ready(function () {
 =======
+=======
+>>>>>>> parent of 7e042aa (Update rss.js)
 =======
 >>>>>>> parent of 7e042aa (Update rss.js)
 =======
@@ -22,7 +25,10 @@ function loadRSS(link, htmlContainer, linktext) {
   feednami.load(url, function(result) {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     debugger
+=======
+>>>>>>> parent of 7e042aa (Update rss.js)
 =======
 >>>>>>> parent of 7e042aa (Update rss.js)
 =======
@@ -54,7 +60,10 @@ function loadRSS(link, htmlContainer, linktext) {
 $(document).ready(function() {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> Stashed changes
+=======
+>>>>>>> parent of 7e042aa (Update rss.js)
 =======
 >>>>>>> parent of 7e042aa (Update rss.js)
 =======
@@ -68,9 +77,12 @@ $(document).ready(function() {
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 $(document).ready(function () {
 =======
+=======
+>>>>>>> parent of 7e042aa (Update rss.js)
 =======
 >>>>>>> parent of 7e042aa (Update rss.js)
 =======
@@ -82,7 +94,10 @@ function loadRSS(link, htmlContainer, linktext) {
   feednami.load(url, function(result) {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     debugger
+=======
+>>>>>>> parent of 7e042aa (Update rss.js)
 =======
 >>>>>>> parent of 7e042aa (Update rss.js)
 =======
@@ -91,6 +106,7 @@ function loadRSS(link, htmlContainer, linktext) {
       console.log(result.error);
     } else {
       var entries = result.feed.entries;
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
       for (var i = 0; i < 8; i++) {
@@ -109,12 +125,21 @@ function loadRSS(link, htmlContainer, linktext) {
         var el = document.createElement( 'html' );
         el.innerHTML = desc;
         //el.innerHTML = '<description><div class="hs-featured-image-wrapper"> <a href="https://www.afpafitness.com/blog/the-role-of-amino-acids-in-promoting-health" title="" class="hs-featured-image-link"> <img src="https://www.afpafitness.com/hubfs/image-from-rawpixel-id-86097-jpeg%20%281%29-1.jpg" alt="The Role of Amino Acids in Promoting Health&nbsp;" class="hs-featured-image" style="width:auto !important; max-width:50%; float:left; margin:0 15px 15px 0;"> </a> </div> <p>While plenty of research has been carried out on the importance of an adequate role of complete proteins, over the past several decades researchers have had a growing interest in the specific roles of protein building blocks, called amino acids, in overall health, metabolism, body composition, and sports performance.</p></description>'
+=======
+      for (var i = 0; i < 12; i++) {
+        var entry = entries[i];
+        var el = document.createElement( 'html' );
+        el.innerHTML = entry.description;
+>>>>>>> parent of 7e042aa (Update rss.js)
         var imgSrc = document.getElementById("mydiv").append( el.getElementsByTagName( 'img' )[0]);
         container.append(
           '<div class="owl-item"><div class="col-md-4 col-sm-6"><div class="card RSS"><img class="card-img-top img-fluid" src="' +
             //entry.enclosures[0].url +
             imgSrc +
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> parent of 7e042aa (Update rss.js)
+=======
 >>>>>>> parent of 7e042aa (Update rss.js)
 =======
 >>>>>>> parent of 7e042aa (Update rss.js)
@@ -136,7 +161,10 @@ function loadRSS(link, htmlContainer, linktext) {
 $(document).ready(function() {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> Stashed changes
+=======
+>>>>>>> parent of 7e042aa (Update rss.js)
 =======
 >>>>>>> parent of 7e042aa (Update rss.js)
 =======
@@ -150,9 +178,12 @@ $(document).ready(function() {
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 <<<<<<< Updated upstream
 $(document).ready(function () {
 =======
+=======
+>>>>>>> parent of 7e042aa (Update rss.js)
 =======
 >>>>>>> parent of 7e042aa (Update rss.js)
 =======
@@ -171,10 +202,13 @@ function loadRSS(link, htmlContainer, linktext) {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         container.append(
           '<div class="col-md-4 col-sm-6"><div class="card RSS"><img class="card-img-top img-fluid" src="' +
             entry.description[0].url +
 =======
+=======
+>>>>>>> parent of 7e042aa (Update rss.js)
 =======
 >>>>>>> parent of 7e042aa (Update rss.js)
         var el = document.createElement( 'html' );
@@ -184,6 +218,7 @@ function loadRSS(link, htmlContainer, linktext) {
           '<div class="col-md-4 col-sm-6"><div class="card RSS"><img class="card-img-top img-fluid" src="' +
             imgSrc +
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> parent of 7e042aa (Update rss.js)
 =======
 >>>>>>> parent of 7e042aa (Update rss.js)
@@ -192,6 +227,8 @@ function loadRSS(link, htmlContainer, linktext) {
           '<div class="col-md-4 col-sm-6"><div class="card RSS"><img class="card-img-top img-fluid" src="' +
             //entry.enclosures[0].url +
 >>>>>>> parent of 08c8f59 (Update rss.js)
+=======
+>>>>>>> parent of 7e042aa (Update rss.js)
             '"><div class="card-body"><h5><a href="' +
             entry.link +
             '"  target="_blank" class="stretched-link">' +
@@ -212,7 +249,10 @@ function loadRSS(link, htmlContainer, linktext) {
 $(document).ready(function() {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> Stashed changes
+=======
+>>>>>>> parent of 7e042aa (Update rss.js)
 =======
 >>>>>>> parent of 7e042aa (Update rss.js)
 =======
@@ -237,7 +277,11 @@ function loadRSS(link, htmlContainer, linktext) {
         var entry = entries[i];
         container.append(
           '<div class="col-md-4 col-sm-6"><div class="card RSS"><img class="card-img-top img-fluid" src="' +
+<<<<<<< HEAD
             //entry.enclosures[0].url +
+=======
+            entry.enclosures[0].url +
+>>>>>>> parent of 7e042aa (Update rss.js)
             '"><div class="card-body"><h5><a href="' +
             entry.link +
             '"  target="_blank" class="stretched-link">' +
